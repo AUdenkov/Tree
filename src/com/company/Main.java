a601c46c534a8b4de0c048c87955fd2e60cdda3a
AUdenkov <92884686+AUdenkov@users.noreply.github.com>
2021-11-15 23:14:48 +0300

    feat: create method tree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Tree tree=new Tree();
-        tree.add(11,"A");
-        tree.add(10,"B");
-        tree.add(5,"V");
-        tree.add(8,"C");
-        tree.add(3,"D");
-        tree.add(6,"E");
-        tree.add(9,"E");
-        tree.print(8);
-        tree.delete(8);
+        Tree tree = new Tree();
+        tree.add(11, "A");
+        tree.add(10, "B");
+        tree.add(5, "V");
+        tree.add(8, "C");
+        tree.add(8, "C");
+        System.out.println(tree.getObject(5));
+
+//        tree.delete(8);
     }
 }
--- src/com/company/Tree.java
+++ src/com/company/Tree.java
@@ -2,77 +2,73 @@ package com.company;
 
 public class Tree {
     private Node root;
-    private Node temp;
-
 
     public void add(int key, Object object) {
+        Node node = new Node(key, object);
         if (root == null) {
-            Node node = new Node(key, object);
             root = node;
         } else {
-            if (key < temp.getKey()) {
-                if (temp.getLeft() == null) {
-                    Node node = new Node(key, object);
-                    temp.setLeft(node);
-                } else {
-                    temp = temp.getLeft();
-                    add(key, object);
-                }
-            } else if (key > temp.getKey()) {
-                if (temp.getRight() == null) {
-                    Node node = new Node(key, object);
-                    temp.setRight(node);
-                } else {
-                    temp = temp.getRight();
-                    add(key, object);
-                }
-            } else {
+            Node temp = search(key, root);
+            if (temp.getKey() == key) {
                 System.out.println("yes key");
+            } else if (key < temp.getKey()) {
+                temp.setLeft(node);
+            } else if (key > temp.getKey()) {
+                temp.setRight(node);
             }
         }
-        temp = root;
     }
 
-    public void delete(int key) {
-        Node node = temp;
-        if (root == null) {
-            System.out.println("No elements");
-        } else if (key == root.getKey()) {
-            root = null;
-            System.out.println("Дерево удалено");
+    private Node search(int key, Node object) {
+        Node node = object;
+        if (key < node.getKey()) {
+            if (node.getLeft() == null) {
+                return node;
+            } else {
+                node = node.getLeft();
+                return search(key, node);
+            }
+        } else if (key > node.getKey()) {
+            if (node.getRight() == null) {
+                return node;
+            } else {
+                node = node.getRight();
+                return search(key, node);
+            }
         } else {
-            if (key < node.getKey()) {
-                if (node.getLeft().getKey() == key || node.getRight().getKey() == key) {
-                    node.setRight(node.getRight().getRight());
-                    temp = node.getRight().getLeft();
-                    add(temp.getKey(), temp.getObject());
-                } else
-                    delete(key);
-            }
-
+            return node;
         }
     }
 
-    public void print(int key) {
-        Node node = temp;
+    public Object getObject(int key) {
         if (root == null) {
-            System.out.println("No elemets");
-        }
-        if (temp == null) {
-            System.out.println("нет числа");
+            return null;
         } else {
-            if (node.getKey() == key) {
-                System.out.println(node);
-            } else if (key < node.getKey()) {
-                temp = node.getLeft();
-                print(key);
-            } else if (key > node.getKey()) {
-                temp = node.getRight();
-                print(key);
-            }
+            Node node = search(key, root);
+            return node.getObject();
         }
-        temp = root;
     }
+
+//    public void delete(int key) {
+//        Node node = temp;
+//        if (root == null) {
+//            System.out.println("No elements");
+//        } else if (key == root.getKey()) {
+//            root = null;
+//            System.out.println("Дерево удалено");
+//        } else {
+//            if (key < node.getKey()) {
+//                if ((node.getLeft().getKey() == key || node.getRight().getKey() == key)) {
+//                    node.setRight(node.getRight().getRight());
+//                    temp = node.getRight().getLeft();
+//                    add(temp.getKey(), temp.getObject());
+//                } else
+//                    delete(key);
+//            }
+//        }
+//    }
+
+
 }
 
 
